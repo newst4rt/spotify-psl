8c06fbe5806b033a7062a8cd89181a39701e3ef7
newst4rt <105312719+newst4rt@users.noreply.github.com>
2025-09-01 23:27:11 +0200

    -

--- main.py
+++ main.py
@@ -18,14 +18,10 @@ def get_track_data(spotify_metadata):
     return str(spotify_metadata["xesam:url"][ix+1:]), artist.replace(" ", "+"), title.replace(" ", "+")
 
 def get_track_position(spotify_metadata):
-    session_bus = dbus.SessionBus()
-    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
-    properties_interface = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties")
-    position = properties_interface.Get("org.mpris.MediaPlayer2.Player", "Position")
+    position_µs = spotify_metadata.Get("org.mpris.MediaPlayer2.Player", "Position") #microseconds
+    position_ms = position_µs / 1_000.0
 
-    position_seconds = position / 1_000.0
-
-    return int(position_seconds)
+    return int(position_ms)
 
 def get_lyrics(artist, title):
     """ Get Lyrics from lrclib.net """
@@ -39,14 +35,14 @@ def get_lyrics(artist, title):
         for x in tmp_lyric_data:
             if x.startswith("["):
                 sep_between_time_and_lyric = x.index("]")
-                times = x[0+1:sep_between_time_and_lyric].split(":")
+                time = x[0+1:sep_between_time_and_lyric].split(":")
                 lyric_line = x[sep_between_time_and_lyric+2:]
-                ms = int(float(times[0])*60*1000 + float(times[1])*1000)
+                ms = int(float(time[0])*60*1000 + float(time[1])*1000)
                 lyric_data_formated.append({"startTimeMs": ms, "lyric_line": lyric_line})
         return lyric_data_formated
     
     elif response.status_code == 404:
-        print(" $! ")
+        c_print("❌")
         return 404
 
 def print_synced_lyric(lyric_data, time_pos):
@@ -61,31 +57,30 @@ def print_synced_lyric(lyric_data, time_pos):
                     c_print(f'♬')
 
 if __name__ == "__main__":
-    #try:
-        session_bus = dbus.SessionBus()
-        spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
-        spotify_metadata = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties")
+    session_bus = dbus.SessionBus()
+    spotify_bus = session_bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+    spotify_metadata = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties")
 
-        id = "initial"
+    id = "initial"
 
-        while True:
-            track_id, artist, title = get_track_data(spotify_metadata)
+    while True:
+        track_id, artist, title = get_track_data(spotify_metadata)
 
-            #Fetch lyrics only when the track has changed
-            if track_id != id:
-                c_print("↻")
-                lyric_data = get_lyrics(artist, title)
-                id, artist, title = get_track_data(spotify_metadata)
-                c_print(f'♬')
-                
-            if lyric_data != 404:
-                time_pos = get_track_position(spotify_metadata)
-                if not lyric_data:
-                    c_print("❌")
-                    sleep(2)
-                else: 
-                    print_synced_lyric(lyric_data, time_pos)
-            else:
-                sleep(0.5)
+        #Fetch lyrics only when the track has changed
+        if track_id != id:
+            c_print("↻")
+            lyric_data = get_lyrics(artist, title)
+            id, artist, title = get_track_data(spotify_metadata)
+            c_print(f'♬')
+            
+        if lyric_data != 404:
+            time_pos = get_track_position(spotify_metadata)
+            if not lyric_data:
+                c_print("❌")
+                sleep(2)
+            else: 
+                print_synced_lyric(lyric_data, time_pos)
+        else:
+            sleep(0.5)
 
-            sleep(0.2)
+        sleep(0.2)
